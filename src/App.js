00a92a7d0bd568ed2cc3662fbdfa91910ca3b65e
hunimoda <hunimoda@gmail.com>
2022-04-11 14:55:25 +0900

    Created NewsList component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=38ab7613a96b4783a2959a8821f86612",
-      );
-      setData(response.data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
